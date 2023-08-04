f52d88a4f85db039954fab8c3d609b31ff1fbcc3
Andrii Hutnyk <andreigutnik@gmail.com>
2023-08-04 17:19:21 +0200

    Update  scrollUp.scss media

--- src/js/components/scrollUp.js
+++ src/js/components/scrollUp.js
@@ -2,18 +2,26 @@ import refs from '../refs';
 
 const { scrollUp } = refs;
 
-scrollUp.addEventListener('click', onClick)
+scrollUp.addEventListener('click', onClick);
 window.addEventListener('scroll', onScroll);
 
 function onScroll() {
-	const scrollY = window.scrollY || document.documentElement.scrollTop
-	if (scrollY > 400) {
-		scrollUp.classList.remove('is-hidden');
-	} else {
-		scrollUp.classList.add('is-hidden');
-	}	
+  const scrollY = window.scrollY || document.documentElement.scrollTop;
+  if (scrollY > 200) {
+    scrollUp.classList.remove('is-hidden');
+  } else {
+    scrollUp.classList.add('is-hidden');
+  }
 }
 
 function onClick() {
-	
+	// if (window.pageYOffset > 0) {
+  //   window.scrollBy(0, -50);
+  //   setTimeout(onClick, 0);
+  // }
+  // window.scrollTo({
+  //   top: 0,
+  //   left: 0,
+  //   behavior: 'smooth',
+  // });
 }
