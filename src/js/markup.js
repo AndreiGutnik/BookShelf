54c052fedab8be3321408f9e2ccc3e9df324ea98
MariaZhuk <shuk.sandypark@gmail.com>
2023-08-06 01:20:19 +0300

    change markup

--- src/js/markup.js
+++ src/js/markup.js
@@ -5,7 +5,7 @@ export default function createCategoryList(arr) {
         .sort((firstTitle, secondTitle) => 
             firstTitle.list_name.localeCompare(secondTitle.list_name))
         .map(({ list_name }) => {
-            return `<li class="category__item">All categories</li>
+            return `
             <li class="category__item">${list_name}
                 </li>`})
             .join("");
