9fef1a6963b2c7e771be3b9b7120d1cce36a06f0
Vika14 <manunya1406@gmail.com>
2023-08-05 20:05:13 +0200

    commit4

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import refs from './js/refs';
 import BooksService from './js/BooksService';
 import LoadMoreBtn from './js/LoadMoreBtn';
 import markupImages from './js/markup';
+import switchThems from './js/switchThems';
 
 const { } = refs;
 
