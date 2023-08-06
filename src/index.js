43229300db1af334d5b02a08535108031c4ea81d
Natalia Popovych <n.gumench@gmail.com>
2023-08-06 10:07:33 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
-import Notiflix from 'notiflix';
-
-import refs from './js/refs';
-import BooksService from './js/BooksService';
-import LoadMoreBtn from './js/LoadMoreBtn';
-import markupImages from './js/markup';
-
-const { } = refs;
-
-
-
-
-
-
-function onError(err) {
-  // loadMoreBtn.hide();
-  Notiflix.Notify.failure(`📌 ${err}`);
-}
+import './js/components/home-page'
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,3 +1,6 @@
-export default {
-
-};
+export default{
+    bookList: document.querySelector('.book-list'),
+    bookList0: document.querySelector('.books0'),
+    mainBody: document.body,
+    loadMore: document.querySelector('.load-more'),
+  };
