209a2c6f566c7e0dc8b6eea619d4fa6dd7fc2199
AndreiGutnik <andreigutnik@gmail.com>
2023-08-07 22:04:02 +0000

    Deploying to gh-pages from @ AndreiGutnik/BookShelf@177ebd5fe25c540cc02e3976a411a75ccb2994f7 🚀

--- index.3d01c04f.js
+++
@@ -1,2 +0,0 @@
-var o={scrollUp:document.querySelector(".scroll-up")};const{scrollUp:e}=o;e.addEventListener("click",(function(){window.scroll({top:0,left:0,behavior:"smooth"})})),window.addEventListener("scroll",(function(){(window.scrollY||document.documentElement.scrollTop)>200?e.classList.remove("is-hidden"):e.classList.add("is-hidden")}));
-//# sourceMappingURL=index.3d01c04f.js.map
--- index.af518494.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={scrollUp:document.querySelector(".scroll-up")}.scrollUp;o.addEventListener("click",(function(){window.scroll({top:0,left:0,behavior:"smooth"})})),window.addEventListener("scroll",(function(){(window.scrollY||document.documentElement.scrollTop)>200?o.classList.remove("is-hidden"):o.classList.add("is-hidden")}))}();
-//# sourceMappingURL=index.af518494.js.map
