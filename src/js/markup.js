199bc64b8f19c5770f69399ebc90a8db0c5c21ec
Andrii Hutnyk <andreigutnik@gmail.com>
2023-08-09 00:43:39 +0200

    Update markup.js

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,15 +1,13 @@
-
-
-
-
-
-
-
-
-
-
-
-
+export function createCategoryList(arr) {
+  return arr
+      .sort((firstTitle, secondTitle) =>
+          firstTitle.list_name.localeCompare(secondTitle.list_name))
+      .map(({ list_name }) => {
+          return `
+          <li class="category__item">${list_name}
+              </li>`})
+          .join("");
+}
 
 export function createMarkupByCategory(arr) {
   return arr
