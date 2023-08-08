f09f35b011737d0ac08d38e5da68a2bf847b67c4
Linka03 <119424683+Linka03@users.noreply.github.com>
2023-08-08 19:40:44 +0300

    Support_UA

--- src/js/components/support_ukraine.js
+++ src/js/components/support_ukraine.js
@@ -1,20 +1,57 @@
 const sliderArrowDown = document.querySelector('.arrow-button');
-let turnButton = true;
-let position = null;
-const sponsorsList = document.querySelector('.fund-info');
+const fundItems = document.querySelectorAll('.fund-item');
+const totalItems = fundItems.length;
+let activeIndex = 0;
+
 sliderArrowDown.addEventListener('click', () => {
-  if (turnButton) {
-    position = 200;
-    turnButton = false;
-    sliderArrowDown.style.transform = 'rotate(180deg)';
+  if (window.innerWidth < 415) {
+    if (activeIndex === 5) {
+      document
+        .querySelector('.fund-info')
+        .scrollTo({ top: 0, behavior: 'smooth' });
+      activeIndex = 0;
+    } else {
+      activeIndex = (activeIndex + 1) % totalItems;
+    }
+  } else if (window.innerWidth >= 415 && window.innerWidth < 1024) {
+    if (activeIndex === 3) {
+      document
+        .querySelector('.fund-info')
+        .scrollTo({ top: 0, behavior: 'smooth' });
+      activeIndex = 0;
+    } else {
+      activeIndex = (activeIndex + 1) % totalItems;
+    }
   } else {
-    position = 0;
-    turnButton = true;
-    sliderArrowDown.style.transform = 'rotate(360deg)';
+    if (activeIndex === 4) {
+      document
+        .querySelector('.fund-info')
+        .scrollTo({ top: 0, behavior: 'smooth' });
+      activeIndex = 0;
+    } else {
+      activeIndex = (activeIndex + 1) % totalItems;
+    }
   }
-  sponsorsList.scroll({
-    left: 0,
-    top: position,
+
+  fundItems[activeIndex].scrollIntoView({
     behavior: 'smooth',
+    block: 'start',
   });
+
+  const arrowIcon = sliderArrowDown.querySelector('.arrow-icon');
+  arrowIcon.classList.toggle('active', activeIndex === totalItems - 1);
+
+  if (window.innerWidth < 415 && activeIndex === 5) {
+    arrowIcon.style.transform = 'rotate(180deg)';
+  } else if (
+    window.innerWidth >= 415 &&
+    window.innerWidth < 1024 &&
+    activeIndex === 3
+  ) {
+    arrowIcon.style.transform = 'rotate(180deg)';
+  } else if (window.innerWidth >= 1024 && activeIndex === 4) {
+    arrowIcon.style.transform = 'rotate(180deg)';
+  } else {
+    arrowIcon.style.transform = 'rotate(0deg)';
+  }
 });
