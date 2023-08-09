a0c391bf934ec1549e3778392c88069daaed6254
OleksiiMinnikov <89975657+OleksiiMinnikov@users.noreply.github.com>
2023-08-09 22:50:49 +0300

    add commit in code JS

--- src/js/about-book-modal.js
+++ src/js/about-book-modal.js
@@ -1,4 +1,4 @@
-// import { openModalId } from './modals';
+// Імпорт необхідних модулів і зображень
 // import { openModalId } from './modals';
 import amazonPng from '../images/png-icons/shops/amazon-icon1x.png';
 import amazonPng2x from '../images/png-icons/shops/amazon-icon2x.png';
@@ -7,22 +7,30 @@ import appleBookPng2x from '../images/png-icons/shops/applebook-icon2x.png';
 import bookShopPng from '../images/png-icons/shops/bookshop-icon1x.png';
 import bookShopPng2x from '../images/png-icons/shops/bookshop-icon2x.png';
 
+// Отримання посилань на елементи DOM
 const allModal = document.querySelector('#allModal');
-const categorieList = document.querySelector('.categorie-list'); 
-const bookList = document.querySelector('.category__books'); 
+const categorieList = document.querySelector('.categorie-list');
+const bookList = document.querySelector('.category__books');
 const storageButton = document.querySelector('.add-storage-button');
 const removeStorageBtn = document.querySelector('.remove-modal-btn');
 const storageDescription = document.querySelector('.storage-description');
+
+// Константа для зберігання даних в локальному сховищі
 const STORAGE_KEY = 'storage-data';
+
+// Масив та об'єкт для зберігання даних
 let storageArr = [];
 let storageObj = {};
 
+// Додавання обробників подій
 storageButton.addEventListener('click', onStorageAdd);
 removeStorageBtn.addEventListener('click', onStorageDelete);
 bookList.addEventListener('click', onIdClick);
-categorieList.addEventListener('click', onIdClick); 
+categorieList.addEventListener('click', onIdClick);
 
+// Функція, яка обробляє клік на елементі списку
 function onIdClick(e) {
+  // Якщо клік відбувся на BUTTON, UL, DIV або H3, ігноруємо його
   if (
     e.target.nodeName === 'BUTTON' ||
     e.target.nodeName === 'UL' ||
@@ -30,31 +38,42 @@ function onIdClick(e) {
     e.target.nodeName === 'H3'
   )
     return;
+
+  // Знаходимо ID ближчого батька <li>
   const id = e.target.closest('li').id;
+  
+  // Виклик функції для відкриття модалки та створення її вмісту
   openModalId();
   createModal(id);
 }
 
+// Асинхронна функція для створення вмісту модалки за допомогою отриманих даних
 async function createModal(bookId) {
   allModal.innerHTML = '';
+
   try {
     const data = await fetchBookById(bookId);
     storageCheck();
     createMarkup(data);
     return data;
   } catch (error) {
-    console.error('Error', error);
+    console.error('Помилка', error);
     throw error;
   }
 }
 
+// Асинхронна функція для отримання даних про книгу за її ID
 async function fetchBookById(bookId) {
   try {
     storageObj = {};
+
+    // Виконуємо запит на сервер
     const response = await fetch(
       `https://books-backend.p.goit.global/books/${bookId}`
     );
     const data = await response.json();
+
+    // Заповнюємо об'єкт даними зі звернення
     storageObj = {
       book_image: data.book_image,
       title: data.title,
@@ -65,12 +84,15 @@ async function fetchBookById(bookId) {
       list_name: data.list_name,
       id: data._id,
     };
+
     return data;
   } catch (error) {
-    console.error('Error', error);
+    console.error('Помилка', error);
     throw error;
   }
 }
+
+// Функція для перевірки наявності даних у локальному сховищі
 function storageCheck() {
   const storageArr = JSON.parse(localStorage.getItem(STORAGE_KEY));
   const idToFind = storageObj.id;
@@ -91,6 +113,7 @@ function storageCheck() {
   }
 }
 
+// Функція для створення розмітки модалки
 function createMarkup(data) {
   const bookModalImage = data.book_image;
   const bookTitle = data.title;
@@ -99,54 +122,52 @@ function createMarkup(data) {
   const marketAppleBooks = data.buy_links[1].url;
   const marketBookshop = data.buy_links[4].url;
 
-  const html = `  
-  <img src="${bookModalImage}" alt="Book Image" class="image-about-book-modal">
-  <div class="info-modal">
-  <h2 class="title-about-book-modal">${bookTitle}</h2>
-  <p class="author-about-book-modal"> ${bookAuthor}</p>
-  <p class="text-about-book-modal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, iure nam facere exercitationem quibusdam cum in quasi impedit perferendis porro. Vero quos minima doloribus magni corporis beatae ducimus officiis! Rerum?</p>
-  <ul class="shop-modal-list"> <li class="shop-modal-item"><a href="${marketAmazon}" target="_blank"
-    > <img
-     width="62"
-    height="19"
-    srcset="
-    ${amazonPng} 1x,
-    ${amazonPng2x} 2x
-  "
-   src="${amazonPng}"
-    alt="Amazon"
-  /></a></li>
-  <li class="shop-modal-item"><a href="${marketAppleBooks}" target="_blank"
-    > <img
-    width="33"
-    height="32"
-    srcset="
-    ${appleBookPng} 1x,
-    ${appleBookPng2x} 2x
-  "
-   src="${appleBookPng}"
-    alt="AppleBooks"
-  /></a></li>
-  <li class="shop-modal-item"><a href="${marketBookshop}" target="_blank"
-    > <img
-    width="38"
-    height="36"
-    srcset="
-    ${bookShopPng} 1x,
-    ${bookShopPng2x} 2x
-  "
-   src="${bookShopPng}"
-    alt="Book-Shop"
-  /></a></li>
-</ul>
-</div>
+  const html = `
+    <img src="${bookModalImage}" alt="Зображення книги" class="image-about-book-modal">
+    <div class="info-modal">
+      <h2 class="title-about-book-modal">${bookTitle}</h2>
+      <p class="author-about-book-modal">${bookAuthor}</p>
+      <p class="text-about-book-modal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, iure nam facere exercitationem quibusdam cum in quasi impedit perferendis porro. Vero quos minima doloribus magni corporis beatae ducimus officiis! Rerum?</p>
+      <ul class="shop-modal-list">
+        <li class="shop-modal-item"><a href="${marketAmazon}" target="_blank">
+          <img
+            width="62"
+            height="19"
+            srcset="${amazonPng} 1x, ${amazonPng2x} 2x"
+            src="${amazonPng}"
+            alt="Amazon"
+          /></a>
+        </li>
+        <li class="shop-modal-item"><a href="${marketAppleBooks}" target="_blank">
+          <img
+            width="33"
+            height="32"
+            srcset="${appleBookPng} 1x, ${appleBookPng2x} 2x"
+            src="${appleBookPng}"
+            alt="AppleBooks"
+          /></a>
+        </li>
+        <li class="shop-modal-item"><a href="${marketBookshop}" target="_blank">
+          <img
+            width="38"
+            height="36"
+            srcset="${bookShopPng} 1x, ${bookShopPng2x} 2x"
+            src="${bookShopPng}"
+            alt="Book-Shop"
+          /></a>
+        </li>
+      </ul>
+    </div>
   `;
+
   allModal.innerHTML = html;
 }
 
+// Функція для додавання даних до локального сховища
 function onStorageAdd() {
   const realStorageArr = JSON.parse(localStorage.getItem(STORAGE_KEY));
   const dataToSave = storageObj;
+
   if (!realStorageArr || realStorageArr.length === 0) {
     storageArr.push(dataToSave);
     localStorage.setItem(STORAGE_KEY, JSON.stringify(storageArr));
@@ -156,10 +177,11 @@ function onStorageAdd() {
   }
 
   storageDescription.textContent =
-    'Сongratulations! You have added the book to the shopping list. To delete, press the button “Remove from the shopping list”.';
+    'Вітаємо! Ви додали книгу до списку покупок. Для видалення натисніть кнопку "Видалити зі списку покупок".';
   storageCheck();
 }
 
+// Функція для видалення даних з локального сховища
 function onStorageDelete() {
   storageDescription.textContent = '';
 
@@ -171,6 +193,5 @@ function onStorageDelete() {
   storageCheck();
 }
 
+// Експортуємо функцію для створення модалки
 export { createModal };
-
-
