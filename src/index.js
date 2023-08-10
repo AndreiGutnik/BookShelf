121239d10cb155ece3a56dcc0902027a82c7f61b
Mykhailo112 <mikhailo.robu@gmail.com>
2023-08-10 21:28:05 +0300

    update shopping list

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import './js/components/authorization.js';
 import './js/components/category.js';
 import './js/components/slider.js';
 import './js/components/home-page';
-import './js/components/switchThems';
-
+import './js/components/switchThems.js';
+import './js/components/shopping_list.js';
 import './js/components/scrollUp.js';
