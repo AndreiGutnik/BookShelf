445a1a1194ac3661b6e50baf5c59265e8df4d46a
Volodymyr <vladikon70@gmail.com>
2023-08-10 18:32:35 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import './js/components/header.js';
 import './js/components/authorization.js';
-import "./js/components/category.js";
+import './js/components/category.js';
 import './js/components/slider.js';
-import './js/components/home-page'
+import './js/components/home-page';
 import './js/components/switchThems';
-
-
-
-
+import './js/components/shopping.js';
 
 import './js/components/scrollUp.js';
