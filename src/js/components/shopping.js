f4e1cf818351252dd30d3bb7f3d9261356d4566e
Volodymyr <vladikon70@gmail.com>
2023-08-11 13:17:26 +0200

    bagfix/shopping_list

--- src/js/components/shopping.js
+++ src/js/components/shopping.js
@@ -11,34 +11,43 @@ if (localStorage.getItem('storage-data')) {
   // CreateMarkup
 
   function makeShoplistMarkup(shoppingList) {
-    const { bookImg, author, listName, description, title, buyLinks, id } =
-      shoppingList;
+    const {
+      book_image,
+      author,
+      list_name,
+      description,
+      title,
+      id,
+      marketAmazon,
+      marketAppleBooks,
+      marketBookshop,
+    } = shoppingList;
 
     const shoplistBookContainer = document.createElement('div');
     shoplistBookContainer.classList.add('shoplist-book-container');
     shoplistBookContainer.dataset.id = id;
 
-    const shoplistMarkup = `<img src="${bookImg}" class="shoplist-book-img">
+    const shoplistMarkup = `<img src="${book_image}" class="shoplist-book-img">
         <div class="shoplist-desc-container">
           <h4 class="shoplist-book-title">${title}</h4>
-          <p class="shoplist-book-genre">${listName}</p>
+          <p class="shoplist-book-genre">${list_name}</p>
           <p class="shoplist-book-description">${description}</p>
            <p class="shoplist-book-author">${author}</p>
         </div>
         <div class="shoplist-icons">
     <ul class="shoplist-icons-list">
       <li class="shoplist-icons-li">
-        <a href="${buyLinks[0].url}" target="blank">
+        <a href="${marketAmazon}" target="blank">
           <div class="shoplist-icon-amazon"></div>
         </a>
       </li>
       <li class="shoplist-icons-li">
-        <a href="${buyLinks[2].url}" target="blank">
+        <a href="${marketAppleBooks}" target="blank">
           <div class="shoplist-icon-barnesAndNoble"></div>
         </a>
       </li>
       <li class="shoplist-icons-li">
-        <a href="${buyLinks[4].url}" target="blank">
+        <a href="${marketBookshop}" target="blank">
           <div class="shoplist-icon-bookshop"></div>
         </a>
       </li>
